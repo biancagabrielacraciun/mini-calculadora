c82c46557fb069b7d0295ee16f72d5c3fc79296d
biancagabrielacraciun <c.biancagabriela171@gmail.com>
2025-11-29 19:06:06 +0100

    Update Pru_480.java

--- Pru_480.java
+++ Pru_480.java
@@ -1,20 +1,16 @@
-
-
-package PqUd3;
-
+//Bianca Gabriela Craciun - Prueba técnica: Mini calculadora de notas.
 import java.util.Scanner;
-
 
 public class Pru_480 {
 
-    
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        int asignaturas, suspendidas, k;
-        double nota, suma, media;
+        int asignaturas, suspendidas, k; 
+        double nota, suma, media; 
         String secuenciaDeNotas;
         boolean aprobado;
+        boolean notaValida;
         
         secuenciaDeNotas = "";
         suma = 0;
@@ -30,27 +26,26 @@ public class Pru_480 {
             {
                 System.out.println("ERROR. El número de asignaturas "
                         + "debe estar entre 1 y 10.");
-            }
-            
+            }   
         }while(!((asignaturas <= 10) && (asignaturas >= 1)));
         
         System.out.println("\nIntroduzca las notas de cada asignatura"
-                + " una por una y con un decimal como mínimo."
-                + " Use la coma(,).\n");
+                + " una por una y con un decimal como mínimo.");
         
-        for(k = 1; k <= asignaturas; k = k+1)
+        for(k = 1; k <= asignaturas; k = k+1) 
         {
             do
             {
                 System.out.print("Introduzca una nota:" + " ");
                 nota = teclado.nextDouble();
                 
-                if(nota < 0 || nota> 10)
+                notaValida = (nota >= 0 && nota <= 10);
+                
+                if(!notaValida) 
                 {
-                    System.out.println("ERROR. La nota debe estar"
-                            + " entre 0 y 10");
+                    System.out.println("ERROR. La nota debe estar entre 0 y 10.");
                 }
-            }while(nota < 0 || nota > 10);
+            }while(!notaValida);
             
             secuenciaDeNotas = (secuenciaDeNotas + nota) + "," + " ";
             suma = suma + nota;
@@ -63,7 +58,7 @@ public class Pru_480 {
         
         media = (suma/asignaturas);
         
-        if(media >= 5)
+        if(media >= 5 && suspendidas <= 1)
         {
             aprobado = true;
         }
@@ -80,7 +75,5 @@ public class Pru_480 {
         {
             System.out.println("Resultado final: HA SUSPENDIDO");
         }
-        
     }
-
 }
